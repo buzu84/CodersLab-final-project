0d1428971a7880bd18e8efff528927bafb7318df
Magdalena Buzikiewicz <buzu@op.pl>
2020-05-31 21:29:11 +0200

    Meteor shower animation in progress.

---
+++ js/MeteorShower.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const MeteorShower = () => {
+
+
+  return (
+    <div className="container">
+      MeteorShower
+    </div>
+  );
+}
+
+export default MeteorShower;

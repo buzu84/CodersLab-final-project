7940cf00a6172178e8a72d3dcc1f59c0d65f0a5d
Magdalena Buzikiewicz <buzu@op.pl>
2020-06-05 15:20:06 +0200

    Feature4 in progress.

--- js/Map.js
+++ js/Map.js
@@ -93,10 +93,11 @@ function Map() {
 const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default function FinalMap() {
+  console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAlOh43-r_92QPINGdzLIAkQzZ9-1iNmbE`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
 const path = require("path");
 const entryPath = "js";
 const entryFile = "App.js";
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: ["whatwg-fetch", `./${entryPath}/${entryFile}`],
-  output: {
-    filename: "out.js",
-    path: path.resolve(__dirname, `${entryPath}/build`)
-  },
-  devServer: {
-    contentBase: path.join(__dirname),
-    publicPath: "/build/",
-    compress: true,
-    port: 3001
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
+module.exports = () => {
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: ["whatwg-fetch", `./${entryPath}/${entryFile}`],
+    output: {
+      filename: "out.js",
+      path: path.resolve(__dirname, `${entryPath}/build`)
+    },
+    devServer: {
+      contentBase: path.join(__dirname),
+      publicPath: "/build/",
+      compress: true,
+      port: 3001
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys)
     ]
   }
 };

fa133ed7f9674378a32a2e033a2b1826ef93bd15
Magdalena Buzikiewicz <buzu@op.pl>
2020-06-09 22:15:01 +0200

    Feature17 finished.

--- js/Astro.js
+++ js/Astro.js
@@ -3,21 +3,36 @@ import ReactDOM from "react-dom";
 import {
   HashRouter,
   Route,
-  Link,
-  Switch,
   NavLink,
+  Switch,
 } from 'react-router-dom';
 import FinalMap from './Map';
 import MeteorShowerDates from './MeteorShowerDates';
-
-
+import PicOfTheDay from './PicOfTheDay';
 
 export default () => {
+
+
   return (
+
     <>
-      <h1 className="app-logo"><span>Astro</span><span>Labium</span></h1>
-      <FinalMap />
-      <MeteorShowerDates />
+      <HashRouter>
+        <header className="header container">
+          <h1 className="logo"><span>Astro</span><span>Labium</span></h1>
+          <nav>
+            <ul>
+              <li><NavLink className="nav_diff" exact to="/astrolabium">MAPA</NavLink></li>
+              <li><NavLink className="nav_diff" to="/astrolabium/picture">NASA OBRAZEK DNIA</NavLink></li>
+              <li><NavLink className="nav_diff" to="/astrolabium/calendar">DESZCZ METEORYTOW</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+            <Route exact path="/astrolabium" component={FinalMap} />
+            <Route path="/astrolabium/calendar" component={MeteorShowerDates} />
+            <Route path="/astrolabium/picture" component={PicOfTheDay} />
+        </Switch>
+      </HashRouter>
     </>
   )
 }
---
+++ js/PicOfTheDay.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const PicOfTheDay = () => {
+  return (
+    <>
+      <h2>NASA Picture</h2>
+    </>
+  );
+}
+
+export default PicOfTheDay;

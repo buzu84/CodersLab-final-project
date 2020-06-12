35dab5b883c04392c9312a37f0a58b9e516ba2dd
Magdalena Buzikiewicz <buzu@op.pl>
2020-06-12 22:11:14 +0200

    Date Picker Component

--- js/PicOfTheDay.js
+++ js/PicOfTheDay.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 const PicOfTheDay = () => {
   const [picture, setPicture] = useState("");
   const [currentDate, setCurrentDate] = useState(new Date().toISOString().split('T')[0]);
-  const [errors, setErrors] = useState([]);
+  // const [errors, setErrors] = useState([]);
 
   useEffect(() => {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&date=${currentDate}`)
@@ -13,27 +15,27 @@ const PicOfTheDay = () => {
     .then(json => setPicture(json))
   }, [currentDate]);
 
-  const changeDate = e => {
-    e.preventDefault();
-    let dateFromInput = document.getElementById('pic_date').value;
-    const validationErrors = validate();
-    if (validationErrors.length !== 0) {
-      setErrors(validationErrors);
-    } else {
-      setCurrentDate(dateFromInput);
-      setErrors([]);
-    }
-  };
+  // const changeDate = e => {
+  //   e.preventDefault();
+  //   let dateFromInput = document.getElementById('pic_date').value;
+  //   const validationErrors = validate();
+  //   if (validationErrors.length !== 0) {
+  //     setErrors(validationErrors);
+  //   } else {
+  //     setCurrentDate(dateFromInput);
+  //     setErrors([]);
+  //   }
+  // };
 
-  const validate = () => {
-    const date = document.getElementById('pic_date').value;
-    const validationErrors = [];
-    const regEx = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
-    if (!date.match(regEx)) {
-      validationErrors.push('Podaj datę w formacie YYYY-MM-DD')
-    }
-      return validationErrors;
-  }
+  // const validate = () => {
+  //   const date = document.getElementById('pic_date').value;
+  //   const validationErrors = [];
+  //   const regEx = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+  //   if (!date.match(regEx)) {
+  //     validationErrors.push('Podaj datę w formacie YYYY-MM-DD')
+  //   }
+  //     return validationErrors;
+  // }
 
   return (
     <div className="container NASAPic_container">
@@ -55,16 +57,14 @@ const PicOfTheDay = () => {
         />
       )}
       <p className="pic_description">{picture.explanation}</p>
-      <form className="pic_date" onSubmit={changeDate}>
-        Może chciałbyś zobaczyć zdjęcia z innych dni? (YYYY-MM-DD):
-        <input id="pic_date"/>
-        <input type="submit" />
-        <ul>
-          {errors.map((error, index) => {
-              return <li key={index}>{error}</li>
-          })}
-        </ul>
-      </form>
+      <DatePicker
+        dateFormat="yyyy-MM-dd"
+        selected={new Date()}
+        onChange={date => setCurrentDate(date.toISOString().split('T')[0])}
+        minDate={new Date("31/5/1996")}
+        maxDate={new Date()}
+        placeholderText="NASA Picture dostępne od 31.05.1996"
+      />
     </div>
   );
 }

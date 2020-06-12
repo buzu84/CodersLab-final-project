56630ea5a95ee1767ac6ca1f3ebf0afbdb79a42e
Magdalena Buzikiewicz <buzu@op.pl>
2020-06-12 13:06:57 +0200

    Feature11 in progress.

--- js/MeteorShowerDates.js
+++ js/MeteorShowerDates.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 
+
 const MeteorShowerDates = () => {
+  const [events, setEvents] = useState([]);
+  const API = "http://localhost:3000";
+
+  useEffect(() => {
+    fetch(`${API}/events`)
+    .then(response => response.json())
+    .then(response => setEvents(response))
+  }, []);
+
   return (
-    <h1>Meteor Shower Dates</h1>
+    <div className="container met_shower_dates">
+      <ul>
+        {events.map(event => {
+          return (
+            <li key={event.id}>
+              <h2 className="event_name">{event.name}</h2>
+              <p>{event.date}</p>
+              <p><span className="peak_hour>">Godzina szczytu roju meteorytów: </span>{event.peakHour}</p>
+              <p>{event.description}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+
   );
 }
 
